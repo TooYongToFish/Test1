3bc583c28ebc7a3eefc514a554ce1d8c7405511e
胡建 <hujianbo@hujianbodemac.local>
2016-02-24 22:23:27 +0800

    Initial Commit

---
+++ aaaaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaaaa
+//
+//  Created by 胡建波 on 16/2/24.
+//  Copyright © 2016年 Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ aaaaa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  aaaaa
+//
+//  Created by 胡建波 on 16/2/24.
+//  Copyright © 2016年 Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
